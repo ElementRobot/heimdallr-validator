405889a433863e2711de1a3cbfea57f875d0f708
jf <jf@elementrobot.com>
2015-05-15 14:21:40 -0700

    Fixed tests

--- tests/test.js
+++ tests/test.js
@@ -1,105 +1,112 @@
+"use strict";
+
 var Mocha = require('mocha'),
     assert = require('chai').assert,
-    validator = require('../index.js'),
-    ts = (new Date()).toISOString(),
+    highland = require('highland'),
+    tv4 = require('tv4'),
+    validator = require('../index.js');
+
+var ts = (new Date()).toISOString(),
     validUUID = 'f52b68f5-4f96-4be2-bf6b-3c78fd29c76d',
+    packetTypes = ['event', 'sensor', 'control'],
     testPackets = {
-        'event': {'subtype': 'test', 'data': true, 't': ts},
-        'sensor': {'subtype': 'test', 'data': true, 't': ts},
-        'control': {
-            'provider': validUUID,
-            'subtype': 'test',
-            'data': true
+        event: {subtype: 'test', data: true, 't': ts},
+        sensor: {subtype: 'test', data: true, 't': ts},
+        control: {
+            provider: validUUID,
+            subtype: 'test',
+            data: true
         }
     };
 
-
-describe('Heimdallr Validator', function(){
-    it('handles valid Heimdallr packet', function(done){
-        for(var packetType in testPackets){
-            validator.validatePacket(packetType, testPackets[packetType]);
-        }
-        done();
-    });
-
-    it('handles invalid Heimdallr packet', function(done){
-        var errorCount = 0;
-        for(var packetType in testPackets){
-            try{
-                validator.validatePacket(packetType, {});
-            }
-            catch(error){
-                errorCount++;
-                assert(
-                    error.expected === 'Heimdallr ' + packetType + ' packet',
-                    error
+describe('Heimdallr Validator', function () {
+    it('handles valid Heimdallr packet', function (done) {
+        var test = highland.wrapCallback(function (packetType, fn) {
+                validator.validatePacket(
+                    packetType,
+                    testPackets[packetType],
+                    fn
                 );
-            }
-        }
-        assert(
-            errorCount === Object.keys(testPackets).length,
-            'incorrect error count'
-        );
-        done();
+            });
+
+        highland(packetTypes).map(test).parallel(9)
+            .errors(function (err) {
+                throw new Error(err);
+            }).done(done);
     });
 
-    it('handles valid data', function(done){
+    it('handles invalid Heimdallr packet', function (done) {
+        var errorCount = 0,
+            test = highland.wrapCallback(function (packetType, fn) {
+                validator.validatePacket(packetType, {}, fn);
+            });
+
+        highland(packetTypes).map(test).parallel(9)
+            .errors(function (err) {
+                errorCount++;
+                assert(err.name === 'ValidationError', err);
+
+                if (errorCount >= Object.keys(testPackets).length) {
+                    done();
+                }
+            }).each(done);
+    });
+
+    it('handles valid data', function (done) {
         validator.validateData(
-            'test', {'title': 'test packet', 'type': 'string'}
+            'test',
+            {title: 'test packet', type: 'string'},
+            done
         );
-        done();
     });
 
-    it('handles invalid data', function(done){
-        var errorCount = 0;
-        try{
-            validator.validateData(
-                'test', {'title': 'test packet', 'type': 'boolean'}
-            );
-        }
-        catch(error){
-            errorCount++;
-            assert(error.expected === 'test packet', error);
-        }
-        assert(errorCount === 1, 'incorrect error count');
-        done();
+    it('handles invalid data', function (done) {
+        validator.validateData(
+            'test',
+            {title: 'test packet', type: 'boolean'},
+            function (err, valid) {
+                assert(err.name === 'ValidationError', err);
+                done();
+            }
+        );
     });
 
-    it('handles invalid UUID', function(done){
+    it('handles invalid UUID', function (done) {
         // xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx where x is a hexadecimal
         // digit and y is 8,9, A, or B
         var errorCount = 0,
-            invalidUUIDs = [
+            invalidUuids = [
                 '52b68f5-8f96-4be2-bf6b-3c78fd29c76d', // wrong length
                 'g52b68f5-8f96-4be2-bf6b-3c78fd29c76d', // non hex digit
                 'f52b68f5-8f96-3be2-bf6b-3c78fd29c76d', // v4
                 'f52b68f5-8f96-4be2-1f6b-3c78fd29c76d' // incorrect y
-            ];
-
-        for(var i = 0; i < invalidUUIDs.length; i++){
-            try{
+            ],
+            test = highland.wrapCallback(function (uuid, fn) {
                 validator.validatePacket(
                     'control',
                     {
-                        'provider': invalidUUIDs[i],
-                        'subtype': 'test',
-                        'data': true
-                    }
+                        provider: uuid,
+                        subtype: 'test',
+                        data: true
+                    },
+                    fn
                 );
+            });
+
+        highland(invalidUuids).map(test).parallel(9).errors(function (err) {
+            errorCount++;
+            assert(
+                err.message === '`provider` must be a valid UUID',
+                'incorrect error message'
+            );
+
+            if (errorCount >= invalidUuids.length) {
+                done();
             }
-            catch(error){
-                errorCount++;
-                assert(
-                    error.message === '`provider` must be a valid UUID',
-                    'incorrect error message'
-                );
-            }
-        }
-        assert(errorCount === invalidUUIDs.length, 'incorrect error count');
-        done();
+        }).each(done);
     });
 
-    it('handles invalid timestamp', function(done){
+    it('handles invalid timestamp', function (done) {
         // ^year-month-dayThour:min:secms?timezone?$
         var errorCount = 0,
             invalidTimestamps = [
@@ -110,29 +117,31 @@ describe('Heimdallr Validator', function(){
                 '2-28-15',
                 '2015-02-28 12:30', // No T
                 '2015-02-28T12:30' // No timezone
-            ];
-
-        for(var i = 0; i < invalidTimestamps.length; i++){
-            try{
+            ],
+            test = highland.wrapCallback(function (timestamp, fn) {
                 validator.validatePacket(
                     'event',
                     {
-                        'subtype': 'test',
-                        'data': true,
-                        't': invalidTimestamps[i]
-                    }
+                        subtype: 'test',
+                        data: true,
+                        t: timestamp
+                    },
+                    fn
                 );
+            });
+
+        highland(invalidTimestamps).map(test).parallel(9).errors(function (err) {
+            errorCount++;
+            assert(
+                err.message === '`t` must ba an ISO 8601 timestamp',
+                'incorrect error message'
+            );
+
+            if (errorCount >= invalidTimestamps.length) {
+                done();
             }
-            catch(error){
-                errorCount++;
-                assert(
-                    error.message === '`t` must ba an ISO 8601 timestamp',
-                    'incorrect error message'
-                );
-            }
-        }
-        assert(errorCount === invalidTimestamps.length, 'incorrect error count');
-        done();
+        }).each(done);
+
     });
 
 });
